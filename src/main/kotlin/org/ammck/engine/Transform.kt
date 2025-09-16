ccd44ffbf8ccd2467865247d040874417083d07d
Anthony McKenzie <ammckenzie1998@gmail.com>
2025-09-16 19:48:06 -0400

    Utility function for getting forward direction

--- src/main/kotlin/org/ammck/engine/Transform.kt
+++ src/main/kotlin/org/ammck/engine/Transform.kt
@@ -7,4 +7,8 @@ data class Transform (
     val position: Vector3f = Vector3f(0.0f, 0.0f, 0.0f),
     val orientation: Quaternionf = Quaternionf(),
     val scale: Vector3f = Vector3f(1.0f, 1.0f, 1.0f)
-)
+){
+    fun forwardDirection(): Vector3f{
+        return Vector3f(0f, 0f, -1f).rotate(this.orientation)
+    }
+}
