0926aa5a3e1fd7e34630fbe80a769cff44d24822
Anthony McKenzie <ammckenzie1998@gmail.com>
2025-09-19 00:02:46 -0400

    Made CarFactory more generic

--- src/main/kotlin/org/ammck/game/factory/CarFactory.kt
+++ src/main/kotlin/org/ammck/game/factory/CarFactory.kt
@@ -9,11 +9,11 @@ import org.ammck.engine.render.Mesh
 import org.ammck.game.Vehicle
 import org.joml.Vector3f
 
-object CarFactory {
+object VehicleFactory {
 
-    fun createPlayerCar(initialTransform: Transform, chassisMesh: Mesh, wheelMesh: Mesh): Vehicle {
-        val playerBoundingBox = AxisAlignedBoundingBox(initialTransform.position, Vector3f(2.0f, 1.0f, 1.0f))
-        val playerBody = PhysicsBody(playerBoundingBox)
+    fun createVehicle(initialTransform: Transform, chassisMesh: Mesh, wheelMesh: Mesh): Vehicle {
+        val boundingBox = AxisAlignedBoundingBox(initialTransform.position, Vector3f(2.0f, 1.0f, 1.0f))
+        val body = PhysicsBody(boundingBox)
 
         val flWheel = createWheelObject(Vector3f(-0.8f,-0.2f,1.5f), wheelMesh)
         val frWheel = createWheelObject(Vector3f(0.8f,-0.2f,1.5f), wheelMesh)
@@ -32,11 +32,11 @@ object CarFactory {
             height = 0.5f
         )
 
-        val playerGameObject = GameObject("Player", initialTransform, chassisMesh, playerBody, suspension)
+        val gameObject = GameObject("placeholder", initialTransform, chassisMesh, body, suspension)
 
-        playerGameObject.addChildren(frWheel, flWheel, rrWheel, rlWheel)
+        gameObject.addChildren(frWheel, flWheel, rrWheel, rlWheel)
 
-        return Vehicle(playerGameObject)
+        return Vehicle(gameObject)
     }
 
     private fun createWheelObject(localPosition: Vector3f, wheelMesh: Mesh): GameObject{
